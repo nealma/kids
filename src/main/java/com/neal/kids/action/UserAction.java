0151b5fc0d1c959d2bf45dc4fb2e516b1b30b02e
neal.ma <neal.ma.sh@gmail.com>
2013-08-30 18:51:28 +0800

    add message

--- src/main/java/com/neal/kids/action/UserAction.java
+++ src/main/java/com/neal/kids/action/UserAction.java
@@ -1,10 +1,14 @@
 package com.neal.kids.action;
 
 import com.neal.kids.bean.User;
+import com.neal.kids.service.IUserService;
+import com.neal.kids.service.impl.UserService;
 import com.neal.mvc.request.RequestContext;
 
 
 public class UserAction{
+	
+	IUserService userService = new UserService();
 
 	/**
 	 * curl http://localhost:8080/kids/action/user/createUser?name=neal
@@ -23,10 +27,10 @@ public class UserAction{
 		System.out.println("run eraseUser");
 	}
 	/**
-	 * curl http://192.168.1.6:8080/kids/action/user/getUser/2
+	 * curl http://192.168.1.6:8080/kids/action/user/getuser
 	 */
-	public void getUser(){
-		RequestContext.get();
+	public void getUsers(){
+		userService.getUsers();
 		System.out.println("run getUser");
 	}
 }
--- src/main/java/com/neal/kids/service/impl/UserService.java
+++ src/main/java/com/neal/kids/service/impl/UserService.java
@@ -1,13 +1,29 @@
 package com.neal.kids.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.neal.kids.bean.User;
 import com.neal.kids.service.IUserService;
 
 public class UserService implements IUserService{
 
 	@Override
-	public String print() {
-		
-		return "Hello world.";
+	public List<User> getUsers() {
+		List<User> users = new ArrayList<User>(10);
+		User user = null;
+		for (int i = 0; i < 10; i++) {
+			user = new User();
+			user.setId(i);
+			user.setName("name"+i);
+			user.setPass("pass"+i);
+			user.setUuid(UUID.randomUUID().toString());
+			users.add(user);
+		}
+		return users;
 	}
+
+	
 	
 }
